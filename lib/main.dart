764a0e0ca5d1a0a81ba12eb5d7bd2e4abeb2a81d
Leonard <leonard@cal-se.com>
2023-04-16 20:16:46 +0700

    Initialize dice app

---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      backgroundColor: Colors.red,
+      appBar: AppBar(
+        title: const Text('Dicee'),
+        backgroundColor: Colors.red,
+      ),
+      body: const DicePage(),
+    ),
+  ));
+}
+
+class DicePage extends StatelessWidget {
+  const DicePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: TextButton(
+              onPressed: () {},
+              child: Image.asset('images/dice1.png'),
+            ),
+          ),
+          Expanded(
+            child: TextButton(
+              onPressed: () {},
+              child: Image.asset('images/dice2.png'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
